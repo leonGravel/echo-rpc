55c02bf38e6041fedb43cb7955834759f3fa46f2
leongravel <leebroncc@gmail.com>
2019-12-04 22:05:42 +0800

    [fix] remove unused import

--- echo-client/src/main/java/com/gravel/echo/client/rpc/RpcFactory.java
+++ echo-client/src/main/java/com/gravel/echo/client/rpc/RpcFactory.java
@@ -1,20 +1,14 @@
 package com.gravel.echo.client.rpc;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.gravel.echo.client.NettyClient;
 import com.gravel.echo.common.entity.Request;
 import com.gravel.echo.common.entity.Response;
-import com.gravel.echo.common.utils.SerializerUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.Map;
 import java.util.UUID;
 
 /**
